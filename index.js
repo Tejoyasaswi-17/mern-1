3d894c95bd2c2db26ed03706d9805cf7de31dc36
Tejoyasaswi-17 <tejoyasaswi1703@gmail.com>
2023-11-19 20:07:16 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from 'mongoose'
+
 const app = express();
+dotenv.config()
+
+const dbConnect = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log('Connection established to MongoDB')
+    } catch(error) {
+        throw error;
+    }
+}
+
+
 
 app.listen(8800, () => {
+    dbConnect();
     console.log('Connection established to backend successfully');
 })
